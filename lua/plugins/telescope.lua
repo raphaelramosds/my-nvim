f0b16c83eb5daecb291472017c60b64343dbbedf
Raphael Ramos <raphael.opensource@gmail.com>
2025-11-29 09:05:24 -0300

    chore: initial commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    tag = "v0.1.9",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    config = function()
+    local builtin = require("telescope.builtin")
+        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<C-n>', builtin.live_grep, {}) -- sudo apt-get install ripgrp
+        vim.keymap.set('n', '<C-k>', ':Neotree filesystem reveal left<CR>', {})
+    end
+}

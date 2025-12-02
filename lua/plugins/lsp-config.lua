0ee66dc4fa6cc76662d5d03ca8a027c2ad46db7c
Raphael Ramos <raphael.opensource@gmail.com>
2025-12-01 22:07:49 -0300

    feat: set completions with luasnip and cmp-nvim-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,58 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            {
-                "mason-org/mason.nvim",
-                opts = {}
-            },
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            require("mason-lspconfig").setup({
-                -- Language Servers
-                ensure_installed = {
-                    "lua_ls",
-                    "ts_ls",
-                    "phpactor"
-                }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.ts_ls.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {},
+			},
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				-- Language Servers
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"phpactor",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.phpactor.setup({
+				capabilities = capabilities,
+			})
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "",
+					severity_sort = true,
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+			})
 
-            vim.diagnostic.config({
-                virtual_text = {
-                    prefix = "",
-                    severity_sort = true,
-                },
-                signs = true,
-                underline = true,
-                update_in_insert = false
-            })
-
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})         -- CTRL + K 
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})   -- CTRL + g + d
-            -- More lsp buf options in :help vim.lsp.buf
-        end
-    }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- CTRL + K
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {}) -- <leader> + g + d
+			-- More lsp buf options in :help vim.lsp.buf
+		end,
+	},
 }
---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,24 @@
+return {
+	"HiPhish/rainbow-delimiters.nvim",
+	config = function()
+		require("rainbow-delimiters.setup").setup({
+			strategy = {
+				[""] = "rainbow-delimiters.strategy.global",
+				vim = "rainbow-delimiters.strategy.local",
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
+			},
+			highlight = {
+				"RainbowDelimiterRed",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		})
+	end,
+}
